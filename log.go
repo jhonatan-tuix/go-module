830e096b54d7d1e0ec70fb934d63568fe316eb44
Jhonatan Caicedo <jhonatan.caicedo@tuix.ch>
2023-08-31 20:54:09 -0500

    feat(): add time stamp

--- log.go
+++ log.go
@@ -2,10 +2,11 @@ package logger
 
 import (
 	"fmt"
+	"time"
 )
 
 var Version string = "1.0"
 
-func Log(mess string) {
-	fmt.Println("[LOG] " + mess)
+func Log(message string) {
+	fmt.Printf("[%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), message)
 }
